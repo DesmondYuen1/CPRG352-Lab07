6145a4b177558f2cec96d6c9f6d7a011ebe6f320
user <user@DESKTOP-FLHNA44.cg.shawcable.net>
2021-11-03 00:21:29 -0600

    Users view refined.

--- src/java/servlets/UserServlet.java
+++ src/java/servlets/UserServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import models.User;
 import services.UserService;
 
@@ -18,31 +17,46 @@ public class UserServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        UserService us = new UserService();
+        String users = "";
 
         try {
-            HttpSession session = request.getSession();
-            String email = (String) session.getAttribute("email");
-            List<User> users = us.getAll();
-            request.setAttribute("users", users);
+            UserService us = new UserService();
+            List<User> usersList = us.getAll();
+            for (int i = 0; i < usersList.size(); i++) {
+                String email = usersList.get(i).getEmail();
+                String active = "";
+                if (usersList.get(i).isActive()) {
+                    active = "active";
+                } else {
+                    active = "inactive";
+                }
+                String first_name = usersList.get(i).getFirst_name();
+                String last_name = usersList.get(i).getLast_name();
+                String role = "";
+                switch (usersList.get(i).getRole()) {
+                    case 1:
+                        role = "system admin";
+                        break;
+                    case 2:
+                        role = "regular user";
+                        break;
+                    default:
+                        role = "company admin";
+                        break;
+                }
+
+                users += "<tr><td>" + email + "</td>" + "<td>" + active + "</td>" + "<td>" + first_name + "</td>" + "<td>" + last_name + "</td>" + "<td>" + role + "</td></tr>";
+
+                request.setAttribute("users", users);
+            }
+
         } catch (Exception ex) {
             Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
-            request.setAttribute("message", "error");
         }
 
-        String action = request.getParameter("action");
-        
-        if (action != null && action.equals("view")) {
-            try {
-                String email = request.getParameter("email");
-                User user  = us.get(email);
-                request.setAttribute("selectedUser", user);
-            } catch (Exception ex) {
-                Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
+        getServletContext()
+                .getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
 
-        getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
         return;
 
     }
--- web/WEB-INF/users.jsp
+++ web/WEB-INF/users.jsp
@@ -1,35 +1,16 @@
-<%@page contentType="text/html" pageEncoding="UTF-8"%>
-<!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <%@page import="java.sql.DriverManager"%>
-        <%@page import="java.sql.ResultSet"%>
-        <%@page import="java.sql.Statement"%>
-        <%@page import="java.sql.Connection"%>
         <title>Users</title>
     </head>
     <body>
-        <h2>Add User</h2>
+
         <div>
-            <%
-                String id = request.getParameter("userId");
-                String driverName = "com.mysql.jdbc.Driver";
-                String connectionUrl = "jdbc:mysql://localhost:3306/";
-                String dbName = "userdb";
-                String userId = "root";
-                String password = "password";
+            <h2>Add User</h2>
 
-                try {
-                    Class.forName(driverName);
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
+        </div>
 
-                Connection connection = null;
-                Statement statement = null;
-                ResultSet resultSet = null;
-            %>
+        <div>
             <h2>Manage Users</h2>
             <table cellpadding="7" border="1">
                 <tr>
@@ -40,35 +21,12 @@
                     <th>Role</th>
                 </tr>
 
-                <%
-                    try {
-                        connection = DriverManager.getConnection(connectionUrl + dbName, userId, password);
-                        statement = connection.createStatement();
-                        String sql = "SELECT * FROM user";
-
-                        resultSet = statement.executeQuery(sql);
-                        while (resultSet.next()) {
-                %>
-                <tr>
-
-                    <td><%=resultSet.getString("email")%></td>
-                    <td><%=resultSet.getString("active")%></td>
-                    <td><%=resultSet.getString("first_name")%></td>
-                    <td><%=resultSet.getString("last_name")%></td>
-                    <td><%=resultSet.getString("role")%></td>
-
-                </tr>
-                <%
-                        }
-
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                %>
+                ${users}
 
 
             </table>
         </div>
-        <h2>Edit User</h2>
-    </body>
-</html>
+
+        <div>
+            <h2>Edit User</h2>
+        </div>
